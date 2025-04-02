7689eba98cbcb890e8792cb6a9cae5fa528c45de
amanraj8797 <125811907+amanraj8797@users.noreply.github.com>
2025-04-02 12:55:45 +0530

    Time: 6 ms (3.26%) | Memory: 42.2 MB (42.78%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.java
@@ -1,28 +1,58 @@
+// class Solution {
+//     public long maximumTripletValue(int[] nums) {
+
+//         long max = 0;
+
+//         int n = nums.length;
+
+//         for(int j = 1; j < n - 1; j++) {
+
+//             int maxI = Integer.MIN_VALUE;
+
+//             for(int i = 0; i < j; i++) {
+//                 maxI = Math.max(maxI, nums[i]);
+//             }
+
+//             int maxK = Integer.MIN_VALUE;
+
+//             for(int k = j + 1; k < n; k++) {
+//                 maxK = Math.max(maxK, nums[k]);
+//             }
+
+//             long cmax = (long)(maxI - nums[j])*maxK;
+
+//             max = Math.max(cmax, max);
+            
+//         }
+
+//         System.out.println(max);
+        
+
+//         return max;
+        
+//     }
+// }
+
+
 class Solution {
     public long maximumTripletValue(int[] nums) {
 
         long max = 0;
 
-        int n = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                for(int k = j + 1; k < nums.length; k++) {
 
-        for(int j = 1; j < n - 1; j++) {
+                    long cmax = (long)(nums[i] - nums[j])*nums[k];
 
-            int maxI = Integer.MIN_VALUE;
+                    if(cmax > max) {
 
-            for(int i = 0; i < j; i++) {
-                maxI = Math.max(maxI, nums[i]);
+                        max = cmax;
+
+                    }
+
+                }
             }
-
-            int maxK = Integer.MIN_VALUE;
-
-            for(int k = j + 1; k < n; k++) {
-                maxK = Math.max(maxK, nums[k]);
-            }
-
-            long cmax = (long)(maxI - nums[j])*maxK;
-
-            max = Math.max(cmax, max);
-            
         }
 
         System.out.println(max);
