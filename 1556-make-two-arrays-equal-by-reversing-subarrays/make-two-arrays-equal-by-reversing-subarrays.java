ccca581d7da8f69ec276848c0451b5a66b50cd4e
amanraj8797 <125811907+amanraj8797@users.noreply.github.com>
2025-04-14 21:01:15 +0530

    Time: 8 ms (20.11%) | Memory: 44.3 MB (64.18%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -9,16 +9,38 @@ class Solution {
         if(a != t) {
             return false;
         }
+// Method 1
+        // Arrays.sort(arr);
+        // Arrays.sort(target);
 
-        Arrays.sort(arr);
-        Arrays.sort(target);
+        // for(int i = 0; i < a; i++) {
+        //     if(arr[i] != target[i]) {
+        //         return false;
+        //     }
+        // }
 
-        for(int i = 0; i < a; i++) {
-            if(arr[i] != target[i]) {
+        // return true;
+
+
+// Method 2
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for(int num: arr) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+
+        for(int num : target) {
+            if(hm.containsKey(num)) {
+                hm.put(num, hm.get(num) - 1);
+            }else {
                 return false;
+            }
+
+            if(hm.get(num) == 0) {
+                hm.remove(num);
             }
         }
 
-        return true;
+        return hm.isEmpty();
     }
 }
