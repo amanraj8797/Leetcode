4a7edf7fff460a17cfc6d3e73294c548a60408d1
amanraj8797 <125811907+amanraj8797@users.noreply.github.com>
2025-04-18 01:00:57 +0530

    Time: 12 ms (5.82%) | Memory: 45 MB (5.19%) - LeetSync

---
+++ 179-largest-number/largest-number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        int n = nums.length;
+        String[] mid = new String[n];
+
+        // Convert each number to string
+        for (int i = 0; i < n; i++) {
+            mid[i] = String.valueOf(nums[i]);
+        }
+
+        // Manual bubble sort based on (a + b).compareTo(b + a)
+        /*
+        also do as:
+        Arrays.sort(mid, (a, b) -> (b + a).compareTo(a + b));
+        */
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                String ij = mid[i] + mid[j];
+                String ji = mid[j] + mid[i];
+                if (ij.compareTo(ji) < 0) {
+                    // Swap
+                    String temp = mid[i];
+                    mid[i] = mid[j];
+                    mid[j] = temp;
+                }
+            }
+        }
+
+        // If the highest number is "0", return "0"
+        if (mid[0].equals("0")) {
+            return "0";
+        }
+
+        // Build the final answer
+        StringBuilder ans = new StringBuilder();
+        for (String s : mid) {
+            ans.append(s);
+        }
+
+        return ans.toString();
+    }
+}
