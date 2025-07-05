7f26203bbce2f6a8fa9cd5ad4d9f523a51fffabb
amanraj8797 <125811907+amanraj8797@users.noreply.github.com>
2025-07-05 16:22:40 +0530

    Time: 1 ms (99.57%) | Memory: 57.1 MB (56.29%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,72 +1,34 @@
-/*
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        Arrays.sort(nums);
-
-        int ans = 0, x = 0;
-
-        HashSet<Integer> hs = new HashSet<>();
-
-        for(int i : nums) {
-            hs.add(i);
-        }
-
-        for (int i : hs) {
-            if(i > 0) {
-                ans += 0;
-            } else {
-                x = i;
-            }            
-        }    
-
-        ans += x;
-
-        return ans;   
-         
-    }
-}
-*/
-
-/*
-class Solution {
-    public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         
-        int res = nums[0];
-        int lastOne = nums[0];
-        
-        for (int i = 1; i < nums.length; i++) {
-            lastOne = Math.max(nums[i], lastOne + nums[i]);
-            res = Math.max(res, lastOne);
-        }
+        int max = nums[0];
 
-        return res;
-    }
-}
-*/
-
-class Solution {
-    public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        
         int n = nums.length;
-        int[] dp = new int[n]; // DP array to store the maximum sum ending at each index
-        
-        dp[0] = nums[0];
-        
-        int res = dp[0];
-        
-        for (int i = 1; i < n; i++) {
-            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
-            res = Math.max(res, dp[i]); 
+
+        /*
+        for(int i = 0; i < n; i++) {
+
+            int mid = 0;
+
+            for(int j = i; j < n; j++) {
+
+                mid += nums[j]; 
+
+                max = Math.max(max, mid);               
+                
+            }
         }
-        
-        return res; 
+        */
+
+        int cur = nums[0];
+
+        for(int i = 1; i < n; i++) {
+
+            cur = Math.max(cur + nums[i], nums[i]);
+
+            max = Math.max(max, cur);
+        }
+
+        return max;
     }
 }
-
