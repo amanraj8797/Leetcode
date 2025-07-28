ba801391c3c1d1faa18fbac87933dda959b93351
amanraj8797 <125811907+amanraj8797@users.noreply.github.com>
2025-07-28 14:46:35 +0530

    Time: 4 ms (50.19%) | Memory: 52.2 MB (19.53%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,18 +1,38 @@
 class Solution {
 
-    public void dfs(char [][] grid, int i, int j, int r, int c) {
+    void doing(char [][] board, int i, int j, int r, int c) {
 
-        if(i < 0 || j < 0 || i >= r || j >= c || grid[i][j] != '1') return;
+        Queue <int []> Q = new LinkedList<>();
 
-        grid[i][j] = '0';
+        int [][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        dfs(grid, i - 1, j, r, c);
-        dfs(grid, i + 1, j, r, c);
-        dfs(grid, i, j - 1, r, c);
-        dfs(grid, i, j + 1, r, c);
+        Q.add(new int[]{i, j});
+
+        board[i][j] = '0';
+
+        while(!Q.isEmpty()) {
+
+            int[] pos = Q.poll();
+
+            int xi = pos[0];
+            int xj = pos[1];
+
+            for(int [] d: dir) {
+
+                int pi = xi + d[0];
+                int pj = xj + d[1];
+
+                if(pi >= 0 && pj >= 0 && pi < r && pj < c && board[pi][pj] == '1') {
+                    Q.add(new int[]{pi, pj});
+                    board[pi][pj] = '0';
+                }
+                
+            }
+        }
 
     }
     public int numIslands(char[][] grid) {
+        
         int r = grid.length;
         int c = grid[0].length;
 
@@ -20,14 +40,13 @@ class Solution {
 
         for(int i = 0; i < r; i++) {
             for(int j = 0; j < c; j++) {
-
                 if(grid[i][j] == '1') {
-                    dfs(grid, i, j, r, c);
                     ans++;
+                    doing(grid, i, j, r, c);
                 }
             }
         }
 
-        return ans;
+        return ans;        
     }
 }
