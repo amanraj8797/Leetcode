2cdab1b7b72dcdf4fad7fb984dc6136b47e6f0ad
amanraj8797 <125811907+amanraj8797@users.noreply.github.com>
2025-08-03 00:49:17 +0530

    Time: 6 ms (48.89%) | Memory: 46.4 MB (6.15%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,52 +1,16 @@
 class Solution {
-
-    boolean binarySearchRow(int [] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
-
-        while(low <= high) {
-            int mid = low + (high - low)/2;
-
-            if(arr[mid] == target) return true;
-            else if(arr[mid] > target) high = mid - 1;
-            else low = mid + 1;
-
-        }
-
-        return false;
-    }
-
-    boolean binarySearchCol(int [][] mat, int i, int j, int target) {
-
-        int low = i;
-        int high = mat.length - 1;
-
-        while(low <= high) {
-
-            int mid = low + (high - low)/2;
-
-            if(mat[mid][j] == target) return true;
-            else if(mat[mid][j] > target) high = mid - 1;
-            else low = mid + 1;
-
-        }
-
-        return false;
-    }
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        int n = matrix.length;
-
+        int row = 0;
         int col = matrix[0].length - 1;
 
-        for(int row = 0; row < n; row++) {
+        while(row < matrix.length && col >= 0) {
 
-            if(matrix[row][0] <= target && matrix[row][col] >= target) {
-                if(binarySearchRow(matrix[row], target) == true) return true;
-            }else if(matrix[row][col] >= target && matrix[n - 1][col] >= target) {
-                if(binarySearchCol(matrix, row, col, target) == true) return true;
-            }
+            if(matrix[row][col] == target) return true;
 
+            else if(matrix[row][col] > target) col--;
+
+            else row++;
         }
 
         return false;
